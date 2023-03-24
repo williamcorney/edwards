a6fd06eb39edefd80d9a90f1c62a9f3e8f231990
ProjectEdwards <william.corney@icloud.com>
2023-03-24 14:53:11 +0000

    Rehashed version of Project which separates out each function by reading/writing to csv files

--- Stage3.py
+++ Stage3.py
@@ -4,30 +4,21 @@ import requests
 import csv
 import time
 
-page = (requests.get('http://edwards.yale.edu/archive?path=aHR0cDovL2Vkd2FyZHMueWFsZS5lZHUvY2dpLWJpbi9uZXdwaGlsby9nZXRvYmplY3QucGw/Yy4xODo3LndqZW8='))
-soup = BeautifulSoup(page.text, "html.parser")
-soup = soup.select ('#content #text')
-if os.path.isfile('test3') is True: os.remove('test3')
-for items in soup:
-    with open('test3', "a") as myfile:
-        myfile.write(str(items))
+#page = (requests.get('http://edwards.yale.edu/archive?path=aHR0cDovL2Vkd2FyZHMueWFsZS5lZHUvY2dpLWJpbi9uZXdwaGlsby9nZXRvYmplY3QucGw/Yy4xODo3LndqZW8='))
+#soup = BeautifulSoup(page.text, "html.parser")
+#soup = soup.select ('#content #text')
+#if os.path.isfile('test3') is True: os.remove('test3')
+#for items in soup:
+ #   with open('test3', "a") as myfile:
+  #      myfile.write(str(items))
 
 with open('test3', 'r') as f:
     contents = f.read()
 soup = BeautifulSoup(contents, "html.parser")
 
-
-
-
 if os.path.isfile('test4.html') is True: os.remove('test4.html')
 for items in soup:
     with open('test4.html', "a") as myfile: myfile.write(str(items))
-
-print (soup)
-#with open('test4.html', 'r') as f:
- #   contents = f.read()
-#soup = BeautifulSoup(contents, "html.parser")
-
 
 for unwanted in soup("center"):
     unwanted.decompose()
@@ -37,7 +28,13 @@ for unwanted in soup("hr"):
         unwanted.decompose()
 if os.path.isfile('test4.html') is True: os.remove('test4.html')
 soup= str(soup)
+
+with open('stylefinal', 'r') as f: style = f.read()
+with open('test4.html', "w") as myfile: myfile.write(str(style))
+
+
 for line in soup.splitlines():
+
         if '</p><p></p>'  in line:
             data =  line.replace('</p><p></p><p>','')
             with open('test4.html', "a") as myfile: myfile.write(str(data))
@@ -45,11 +42,6 @@ for line in soup.splitlines():
         if '</p><p></p><p>' not in line:
             data = (line)
             with open('test4.html', "a") as myfile: myfile.write(str(data))
-
-# with open('test4.html', "a") as myfile: myfile.write(str(items))
-
-
- #   print (str(items))
 
 import webbrowser
 webbrowser.open('file:///Users/williamcorney/Edwards/test4.html', new=2)
