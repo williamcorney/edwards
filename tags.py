caf3e9897ddb436dbca2372cf3f3247d2b42f819
williamcorney <118494245+williamcorney@users.noreply.github.com>
2023-04-10 01:17:13 +0100

    Update tags.py

--- tags.py
+++ tags.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 from html.entities import name2codepoint
 from colorama import Fore, Back, Style
 import time
-
-
-
 with open('cache', 'r') as f: contents = f.read()
 soup = BeautifulSoup(contents, "html.parser")
+
+import docx
+
+document = docx.Document()
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -24,23 +25,34 @@ class MyHTMLParser(HTMLParser):
 
         print (Fore.WHITE + Back.BLACK)
         for attr in attrs:
+
             if 'fnote' in attr:
                 self.flag = 'fnote'
             if 'head' in attr:
                 self.flag = 'head'
+            if '1' in attr:
+                self.flag = '1'
+        if len(tag) < 2 :
+            self.flag = 'p'
+            self.tag = ''
 
     def handle_endtag(self, tag):
         print(Fore.RED + "End tag:", tag.upper())
 
     def handle_data(self, data):
-            if 'fnote' in self.flag:
-                print(Fore.BLUE + "Data     :", data)
-            if 'head' in self.flag:
-                print(Fore.LIGHTGREEN_EX + "Data     :", data)
-            else:
-                print(Fore.YELLOW + "Data     :", data)
 
-
+        length = len (data)
+        if length > 2:
+            print(Fore.LIGHTGREEN_EX + "Data     :", data)
+            print (self.flag)
+        #if 'head' in self.flag:
+         #   print ('blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla')
+         #   document.add_heading(data, 0)
+        if 'p' in self.flag:
+            print(Fore.LIGHTYELLOW_EX + " Flag was P")
+            p = document.add_paragraph(data)
+        if 'head' in self.flag:
+            document.add_heading(data, 0)
 
 for tag in soup.find_all(attrs={'style': True}):
     del tag['style']
@@ -71,8 +83,25 @@ for items in div_tags:
     if 'class' not in str(items):
         items.unwrap()
 
+
+mydivs = soup.find_all("span", {"class": "bibl"})
+
+for items in mydivs:
+    items.unwrap()
+
+mydivs = soup.find_all("span", {"class": "reg"})
+
+for items in mydivs:
+    items.unwrap()
+
 #print (soup)
+
+#soup = soup.prettify()
 
 parser.feed(str(soup))
 
 with open("test.html", "w") as myfile: myfile.write(str(soup))
+
+#soup = soup.select('.span , .bibl')
+
+document.save('testing.docx')
