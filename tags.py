c489240c3acaeb0ff8bf71e160307c0fb79f070e
williamcorney <118494245+williamcorney@users.noreply.github.com>
2023-04-11 16:14:47 +0100

    Update tags.py

--- tags.py
+++ tags.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import docx, os
-
-
+import subprocess
+#pkill - x "Microsoft Word"
+subprocess.call('pkill -x "Microsoft Word"', shell=True)
 
 # STAGE 0 ---LOAD UP SOUP FILE
 with open('sermon.html', 'r') as f: contents = f.read()
@@ -12,13 +13,25 @@ soup = BeautifulSoup(contents, "lxml")
 from colorama import Fore, Back, Style
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
+
+
+        self.count = 0
+
         if len (attrs) == 0 :
            #print(Fore.LIGHTGREEN_EX + 'start of ' + tag)
-            self.flag = tag
+           self.flag = tag
+
+
         else:
             for item in attrs:
+
                 #print (Fore.LIGHTGREEN_EX + tag + ' (' + item[1] + ')')
+                print ('TAG ASSIGNMENT SECTION')
                 self.flag = tag + '-' + item[1]
+                if item[1] == 'head':
+                    print ('TRIGGER ACTIVATED')
+                    self.headswitch =1
+
 
         #for item in attrs:
             #print ( tag  + item[1] + 'ZZZZZZ' )
@@ -32,6 +45,38 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         print (Fore.LIGHTGREEN_EX + self.flag)
         print (Fore.LIGHTBLUE_EX + data )
+
+        if self.flag == 'span-head':
+
+            self.paragraph = document.add_heading(data, 3)
+            self.paragraph = document.add_paragraph('')
+
+        if self.flag == 'p':
+            self.paragraph = document.add_paragraph (data)
+
+        if self.flag == 'span-fnote':
+
+            self.paragraph.add_footnote(data) # add a footnote
+        if self.flag == 'span-font-style:italic':
+            if self.count == 1:
+                self.paragraph.add_run(data)
+            if self.count == 0 :
+                self.count = self.count + 1
+                self.paragraph.add_run(data).italic=True
+
+
+
+        if self.flag == 'b':
+            self.paragraph.add_run(data).bold = True
+        if self.flag == 'span-bibl':
+            if self.count == 1:
+                self.paragraph.add_run(data)
+            if self.count == 0:
+                self.count = self.count + 1
+                self.paragraph.add_run(data).italic = True
+
+        #pkill - x "Microsoft Word"
+
 
 # ******** TRANSFORMATIONS********
 
@@ -59,7 +104,7 @@ if len (tagunwrap) > 1 :
 
 # STAGE 3 ---UNWRAP SPAN TAGS BY CLASS
 
-classnames = ['reg']
+classnames = ['reg','epigraph']
 if len (classnames) == 1 :
     for span_tag in soup.findAll('span', {'class': classnames}): span_tag.unwrap()
 if len (classnames) > 1 :
@@ -78,35 +123,44 @@ for items in soup.select(thenameofclass):
 
 #print (soup.prettify())
 
-# STAGE 5 - SEARCH FOR TAGS BY TAG NAME
-taglist = [tag.name for tag in soup.find_all()]
-list = []
-for items in taglist:
-    if items not in list:
-        list.append(items)
-print (list)
-
-#STAGE 6 - SEARCH FOR SPAN TAGS BY  CLASSNAME
-
-searchterm = ['']
-
-searchresponse= (soup.findAll('span', {'class':searchterm}))
-
-for items in searchresponse: print (items)
-
-#STAGE 7 - SEARCH FOR SPECIFIC TAG
-
-tagquery=['']
-for items in soup.find_all(tagquery): print (items)
-#searchterm = 'span'
-#for items in soup.find_all(searchterm): print (items)
-
-# STAGE 8 ---CREATE DOCX OBJECT
+# STAGE 5 ---CREATE DOCX OBJECT
 if os.path.isfile('testing.docx') is True: os.remove('testing.docx')
 document = docx.Document()
 MyHTMLParser().feed(str(soup))
 document.save('testing.docx')
 
+
+# STAGE 6 - SHOW ALL TAGS IN DOCUMENT
+list=[]  #create a list , not for search usage!
+taglist = [tag.name for tag in soup.find_all()]
+for items in taglist:
+    if items not in list:
+        list.append(items)
+print (list)
+
+#STAGE 7 - SEARCH FOR SPAN TAGS BY  CLASSNAME
+
+searchterm = ['font-style:italic']
+
+searchresponse= (soup.findAll('span', {'class':searchterm}))
+
+print (searchresponse)
+print ('hello!!!!')
+for items in searchresponse: print (items)
+
+#STAGE 8 - SEARCH FOR SPECIFIC TAG
+
+tagquery=['']
+if len(tagquery) < 2:
+    print (soup.find_all(tagquery))
+if len(tagquery) > 1:
+    for items in soup.find_all(tagquery): print (items)
+#searchterm = 'span'
+#for items in soup.find_all(searchterm): print (items)
+
+
+
+subprocess.call('open -a "Microsoft Word" /Users/williamcorney/PycharmProjects/Tags/testing.docx',shell=True)
 
 if os.path.isfile('test.html') is True: os.remove('test.html')
 for items in soup:
